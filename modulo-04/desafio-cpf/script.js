0f93703e38ebeb2ffcbab2d6cb47f0efcb287d0d
Maju <majutrevisan@gmail.com>
2021-08-02 15:57:41 -0300

    Módulo 4 - Validador de CPF - Parte II

--- modulo-04/desafio-cpf/script.js
+++ modulo-04/desafio-cpf/script.js
@@ -1,3 +1,17 @@
-function validarCpf(){
+function validacao(){
     console.log('Iniciando função de validação.');
+
+    let cpf = document.getElementById('cpf_digitado').value;
+    
+    let resultadoDaValidacao = validaCpf(cpf);
+
+    if (resultadoDaValidacao) {
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block'
+    }
+}
+
+function validaCpf(cpf){
+    return false;
 }
